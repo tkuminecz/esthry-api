1114e8e1a3b22b5c51458ca0e3d2ffaddf2d278a
Tim Kuminecz <tkuminecz@gmail.com>
2015-03-23 23:00:54 -0400

    cache image post tranforms

--- src/asset/display.js
+++ src/asset/display.js
@@ -1,14 +1,17 @@
 /* asset/display.js */
 
-import Cache from 'asset/cache';
+import Cache from 'cache';
 import Route from 'routes';
 import get from 'asset/get';
 import lwip from 'lwip';
+import md5 from 'MD5';
 import mongo from 'db';
 import q from 'q';
 import request from 'request';
 import transform from 'transform';
 import validation from 'validation';
+
+var cache_version = 1;
 
 /**
  * Retrieves base64 from the given url
@@ -36,43 +39,58 @@ function getBase64Data(url) {
  */
 function display(id, res) {
 	var cache = new Cache(),
-		cache_key = `asset-${id}`;
+		cache_key;
 
 	validation.validateValue({validate: 'objectid'}, id);
 
 	return get(id)
 		.then(img_data => {
+			var defer = q.defer();
+
+			cache_key = md5(cache_version + JSON.stringify([img_data.s3_url, img_data.type, img_data.transforms]));
+
+			process.stdout.write("\t+ getting image data...");
 
 			// attempt to get data from cache
-			return cache.get(cache_key)
+			cache.get(cache_key)
 				.then(cache_data => {
+					var buffer;
+
 					if (cache_data) {
-						return q(cache_data);
+						buffer = new Buffer(cache_data, 'base64');
+						console.log('cached...done.');
+						process.stdout.write("\t+ sending response...");
+						Route.sendMimeData(res, img_data.type, buffer.length, buffer);
+						defer.resolve();
+						console.log('done.');
 					}
 					else {
-						return getBase64Data(img_data.s3_url)
+						process.stdout.write('aws...');
+						getBase64Data(img_data.s3_url)
 							.then(base64_data => {
-								// save data to cache
-								cache.add(cache_key, base64_data);
+								var buffer = new Buffer(base64_data, 'base64');
 
-								return base64_data;
+								console.log('done.');
+
+								// apply image transformations
+								transform(buffer, img_data.type.split('/')[1], img_data.transforms)
+									.then(buffer => {
+										// save data to cache
+										process.stdout.write("\t+ saving to cache...");
+										cache.add(cache_key, buffer.toString('base64'));
+										console.log('done.');
+
+										// send image to browser
+										process.stdout.write("\t+ sending response...");
+										Route.sendMimeData(res, img_data.type, buffer.length, buffer);
+										defer.resolve();
+										console.log('done.');
+									});
 							});
 					}
-				})
-				.then(base64_data => {
-					var defer = q.defer(),
-						buffer = new Buffer(base64_data, 'base64');
-
-					// apply image transformations
-					transform(buffer, img_data.type.split('/')[1], img_data.transforms)
-						.then(buffer => {
-							// send image to browser
-							Route.sendMimeData(res, img_data.type, buffer.length, buffer);
-							defer.resolve();
-						});
-
-					return defer.promise;
 				});
+
+			return defer.promise;
 		});
 }
 
